6edbc275f728495dfd53e4720c69a0fdac30584d
Charles Lambret <charleslambretpro@gmail.com>
2024-06-27 15:29:52 +0200

    rajout de tests dupliqués à partir du premier

--- src/test/java/fr/hetic/ConvertisseurRomainTest.java
+++ src/test/java/fr/hetic/ConvertisseurRomainTest.java
@@ -6,9 +6,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ConvertisseurRomainTest {
 
     @Test
-    public void doitConvertir1EnI() {
+    public void doitConvertir2EnII() {
         ConvertisseurRomain convertisseur = new ConvertisseurRomain();
-        String resultat = convertisseur.convertir(1);
-        assertThat(resultat).isEqualTo("I");
+        String resultat = convertisseur.convertir(2);
+        assertThat(resultat).isEqualTo("II");
+    }
+
+    @Test
+    public void doitConvertir6EnVI() {
+        ConvertisseurRomain convertisseur = new ConvertisseurRomain();
+        String resultat = convertisseur.convertir(6);
+        assertThat(resultat).isEqualTo("VI");
+    }
+
+    @Test
+    public void doitConvertir13EnXIII() {
+        ConvertisseurRomain convertisseur = new ConvertisseurRomain();
+        String resultat = convertisseur.convertir(13);
+        assertThat(resultat).isEqualTo("XIII");
+    }
+
+    @Test
+    public void doitConvertir30EnXXX() {
+        ConvertisseurRomain convertisseur = new ConvertisseurRomain();
+        String resultat = convertisseur.convertir(30);
+        assertThat(resultat).isEqualTo("XXX");
     }
 }
