7f0acb1b790056f6eaa9e29761bcbfabf9d77dec
Charles Lambret <charleslambretpro@gmail.com>
2024-06-27 15:45:11 +0200

    rajout des grands nombres aux dictionnaire pour la gestion des exceptions

--- src/main/java/fr/hetic/ConvertisseurRomain.java
+++ src/main/java/fr/hetic/ConvertisseurRomain.java
@@ -5,8 +5,8 @@ public class ConvertisseurRomain {
     public String convertir(int nombre) {
         StringBuilder resultat = new StringBuilder();
 
-        int[] valeurs = {10, 9, 5, 4, 1};
-        String[] symboles = {"X", "IX", "V", "IV", "I"};
+        int[] valeurs = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symboles = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
         for (int i = 0; i < valeurs.length; i++) {
             while (nombre >= valeurs[i]) {
