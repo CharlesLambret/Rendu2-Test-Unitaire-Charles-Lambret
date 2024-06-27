fd56fb8e846542ea04e636b72699185b78719926
Charles Lambret <charleslambretpro@gmail.com>
2024-06-27 15:57:18 +0200

    utilisation de la méthode repeat pour que le traitement de chaque signe romain soit plus clair

--- src/main/java/fr/hetic/ConvertisseurRomain.java
+++ src/main/java/fr/hetic/ConvertisseurRomain.java
@@ -3,17 +3,61 @@ package fr.hetic;
 public class ConvertisseurRomain {
 
     public String convertir(int nombre) {
-        StringBuilder resultat = new StringBuilder();
-
-        int[] valeurs = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] symboles = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-
-        for (int i = 0; i < valeurs.length; i++) {
-            while (nombre >= valeurs[i]) {
-                resultat.append(symboles[i]);
-                nombre -= valeurs[i];
-            }
+        if (nombre <= 0 || nombre > 3000) {
+            throw new IllegalArgumentException("Le nombre doit être compris entre 1 et 3000");
         }
+
+        StringBuilder resultat = new StringBuilder();
+        
+        resultat.append("M".repeat(nombre / 1000));
+        nombre %= 1000;
+
+        if (nombre >= 900) {
+            resultat.append("CM");
+            nombre -= 900;
+        }
+
+        resultat.append("D".repeat(nombre / 500));
+        nombre %= 500;
+
+        if (nombre >= 400) {
+            resultat.append("CD");
+            nombre -= 400;
+        }
+
+        resultat.append("C".repeat(nombre / 100));
+        nombre %= 100;
+
+        if (nombre >= 90) {
+            resultat.append("XC");
+            nombre -= 90;
+        }
+
+        resultat.append("L".repeat(nombre / 50));
+        nombre %= 50;
+
+        if (nombre >= 40) {
+            resultat.append("XL");
+            nombre -= 40;
+        }
+
+        resultat.append("X".repeat(nombre / 10));
+        nombre %= 10;
+
+        if (nombre >= 9) {
+            resultat.append("IX");
+            nombre -= 9;
+        }
+
+        resultat.append("V".repeat(nombre / 5));
+        nombre %= 5;
+
+        if (nombre >= 4) {
+            resultat.append("IV");
+            nombre -= 4;
+        }
+
+        resultat.append("I".repeat(nombre));
 
         return resultat.toString();
     }
