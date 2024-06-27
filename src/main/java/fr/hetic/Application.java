7a6b743f484c6b4c67c845113c480e94113b6af0
Charles Lambret <charleslambretpro@gmail.com>
2024-06-27 16:10:08 +0200

    mise à jour des messages d'erreurs pour passer les tests

--- src/main/java/fr/hetic/Application.java
+++ src/main/java/fr/hetic/Application.java
@@ -1,20 +1,23 @@
 package fr.hetic;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ConvertisseurRomain convertisseur = new ConvertisseurRomain();
+        try (Scanner scanner = new Scanner(System.in)) {
+            ConvertisseurRomain convertisseur = new ConvertisseurRomain();
 
-        System.out.println("Entrez un nombre entre 1 et 2000 : ");
-        int nombre = scanner.nextInt();
-
-        try {
-            String resultat = convertisseur.convertir(nombre);
-            System.out.println("Le chiffre romain est : " + resultat);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Entrez un nombre entre 1 et 2000 : ");
+            try {
+                int nombre = scanner.nextInt();
+                String resultat = convertisseur.convertir(nombre);
+                System.out.println("Le chiffre romain est : " + resultat);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            } catch (InputMismatchException e) {
+                System.out.println("Veuillez entrer un nombre valide.");
+            }
         }
     }
 }
